9c19fcd0206944a17bf0a1810286778c7a78043f
Marko Lozančić <markolozancic@Markos-iMac.local>
2019-06-04 16:32:15 +0200

    routing implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import Navbar from './components/Navbar' 
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Blog from "./components/Blog";
+import News from "./components/News";
+import Weather from "./components/Weather";
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Navbar></Navbar>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Route exact path='/' component={ News } />
+        <Route path='/weather' component={ Weather } />
+        <Route path='/blog' component={ Blog } />
+      </div>
+    </BrowserRouter>
   );
 }
 
